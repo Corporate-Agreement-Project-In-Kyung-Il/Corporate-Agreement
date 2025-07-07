1d894aa0c0a93f47d29edafd25fea49871133f15
kot775 <kot775@naver.com>
2025-07-07 10:26:32 +0900

    [엑셀 읽어오는 스크립트 수정]

--- Assets/03.Script/DataTableReader/DataTableReader_Example.cs
+++ Assets/03.Script/DataTableReader/DataTableReader_Example.cs
@@ -5,21 +5,22 @@ using ExcelDataReader;
 
 public class DataTableReader_Example : MonoBehaviour
 {
+    private int rowCount;
     void Start()
     {
         // 인코딩 등록 (한글 깨짐 방지)
         System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
         // 엑셀 파일 경로
-        string filePath = Application.dataPath + "/DataTable/성장 데이터 종합 v1.1.xlsx";
+        string filePath = Application.dataPath + "/05.DataTable/성장 데이터 종합 v1.1.xlsx";
 
         // 엑셀 열기
         using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
         using var reader = ExcelReaderFactory.CreateReader(stream);
         var result = reader.AsDataSet();
 
-        // 첫 번째 시트 가져오기
-        DataTable table = result.Tables[0];
+        // N 번째 시트 가져오기
+        DataTable table = result.Tables[4];
 
         // 첫 번째 행부터 전체 출력
         for (int i = 0; i < table.Rows.Count; i++)
@@ -27,7 +28,13 @@ public class DataTableReader_Example : MonoBehaviour
             string rowData = "";
             for (int j = 0; j < table.Columns.Count; j++)
             {
-                rowData += table.Rows[i][j].ToString() + " | ";
+                var cell = table.Rows[i][j]?.ToString();
+                if (!string.IsNullOrEmpty(cell))
+                {
+                    if (!string.IsNullOrEmpty(rowData))
+                        rowData += " | ";
+                    rowData += cell;
+                }
             }
             Debug.Log(rowData);
         }
