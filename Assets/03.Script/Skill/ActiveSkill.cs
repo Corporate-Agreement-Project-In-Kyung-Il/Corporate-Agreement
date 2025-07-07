982286ca7bd653e044511651b6501606b91cfae8
DESKTOP-O34IBGR\KGA <cozam2@naver.com>
2025-07-07 13:41:33 +0900

    스킬데이터클래스

---
+++ Assets/03.Script/Skill/ActiveSkill.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveSkill : MonoBehaviour
+{
+    public ActiveSkillData skillData=new ActiveSkillData();
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
