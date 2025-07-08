1b63e538b556ea34111e3c6fa9fb48fdbb18fabb
kot775 <kot775@naver.com>
2025-07-08 14:20:35 +0900

    [선택지 고르기 완성]
    랜덤 선택지의 SelectionID 뽑아내기 완료

--- Assets/03.Script/OptionChoiceButton.cs
+++ Assets/03.Script/OptionChoiceButton.cs
@@ -2,11 +2,85 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class OptionChoiceButton : MonoBehaviour
 {
-    private void Start()
+    public ScriptableObject skillOption;
+    public ScriptableObject equipOption;
+    public ScriptableObject trainingOption;
+    public int choiceCount = 3;
+    public enum EOptionType
     {
-        throw new NotImplementedException();
+        Skill,
+        Equip,
+        Training
+    }
+    Dictionary<Enum, ScriptableObject> m_Options = new Dictionary<Enum, ScriptableObject>();
+    private void Awake()
+    {
+        m_Options.Add(EOptionType.Skill, skillOption);
+        m_Options.Add(EOptionType.Equip, equipOption);
+        m_Options.Add(EOptionType.Training, trainingOption);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateChoices();
+        }
+    }
+
+    private void CreateChoices()
+    {
+        for (int i = 0; i < choiceCount; i++)
+        {
+            EOptionType choicedOption = GetOptionType();
+            if (m_Options.TryGetValue(choicedOption, out ScriptableObject option))
+            {
+                switch (choicedOption)
+                {
+                    case EOptionType.Skill:
+                        var skill = option as OptionChoice_Skill;
+                        int skillID = GetSelectionID(skill);
+                        Debug.Log("Skill 선택됨: " + skillID);
+                        break;
+                    case EOptionType.Equip:
+                        var equip = option as OptionChoice_Equip;
+                        int equipID = GetSelectionID(equip);
+                        Debug.Log("Equip 선택됨: " + equipID);
+                        break;
+                    case EOptionType.Training:
+                        var training = option as OptionChoice_Training;
+                        int trainingID = GetSelectionID(training);
+                        Debug.Log("Training 선택됨: " + trainingID);
+                        break;
+                }
+            }
+            else
+            {
+                Debug.LogError("No option found for type: " + choicedOption);
+            }
+        }
+    }
+
+    EOptionType GetOptionType()
+    {
+        EOptionType[] values = (EOptionType[])System.Enum.GetValues(typeof(EOptionType));
+        int randomIndex = UnityEngine.Random.Range(0, values.Length);
+        return values[randomIndex];
+    }
+    
+    int GetSelectionID<T>(OptionChoiceBase<T> option) where T : BaseValue, new()
+    {
+        if (option == null || option.data == null || option.data.Count == 0)
+        {
+            Debug.LogWarning("옵션이 null이거나 비어있음");
+            return -1;
+        }
+
+        int randomIndex = UnityEngine.Random.Range(0, option.data.Count);
+        return option.data[randomIndex].Selection_ID; // IDValuePair<T> 에 id가 있다고 가정
     }
 }
