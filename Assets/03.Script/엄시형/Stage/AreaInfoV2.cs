7e081daa3e893200851b390092c3ca640e089807
lilill16171df <luna17f@naver.com>
2025-07-08 18:00:21 +0900

    [에디터에서 스폰위치 정하게 수정중]

--- Assets/03.Script/엄시형/Stage/AreaInfoV2.cs
+++ Assets/03.Script/엄시형/Stage/AreaInfoV2.cs
@@ -6,20 +6,20 @@ using UnityEngine.Serialization;
 
 namespace _03.Script.엄시형.Data.V2
 {
-    [Serializable]
-    public class AreaInfo
+    [CreateAssetMenu(fileName = "AreaInfoSO", menuName = "SO/Stage/AreaInfoSO", order = 1)]
+    public class AreaInfoSO : ScriptableObject
     {
-        public bool bIsBossStage =>  mbIsBossStage;
+        // public bool bIsBossStage =>  mbIsBossStage;
         public int MonsterCount => mMonsterSpawnPointList.Count;
-        public List<MonsterType> SpawnMonsterTypeList => mSpawnMonsterTypeList;
+        // public List<MonsterType> SpawnMonsterTypeList => mSpawnMonsterTypeList;
         public List<Vector2Int> MonsterSpawnPointList => mMonsterSpawnPointList;
         
-        [Header("등장 몬스터!")]
-        [SerializeField] private List<MonsterType> mSpawnMonsterTypeList;
+        // [Header("등장 몬스터!")]
+        // [SerializeField] private List<MonsterType> mSpawnMonsterTypeList;
         
         [Header("몬스터 스폰지점")]
         [SerializeField] private List<Vector2Int> mMonsterSpawnPointList;
         
-        private bool mbIsBossStage;
+        // private bool mbIsBossStage;
     }
 }
--- Assets/03.Script/엄시형/Stage/StageInfoSO.cs
+++ Assets/03.Script/엄시형/Stage/StageInfoSO.cs
@@ -1,30 +1,32 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using _03.Script.엄시형.Data.V2;
+using _03.Script.엄시형.Monster;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-[CreateAssetMenu(fileName = "StageInfo", menuName = "SO/Stage/StageInfo", order = 1)]
-public class StageInfoSO : ScriptableObject
+namespace _03.Script.엄시형.Stage
 {
-    // TODO : 엑셀이나 CSV부터 읽어오기 + 에디터에 올리기
-    public List<AreaInfo> AreaInfoList => mAreaInfoList;
-    
-    [Header("현재 스테이지")]
-    [SerializeField] private int mStageIndex;
-
-    [Header("몬스터 정보")]
-    [SerializeField] private float mMonsterHp = 100f;
-    [SerializeField] private float mMonsterAttack = 10f;
-    
-    [Header("구역 정보 1구역 2구역 3구역 4구역(보스스테이지)")]
-    [SerializeField] private List<AreaInfo> mAreaInfoList;
-    
-    
-    private void OnValidate()
+    [CreateAssetMenu(fileName = "StageInfo", menuName = "SO/Stage/StageInfo", order = 1)]
+    public class StageInfoSo : ScriptableObject
     {
-        Debug.Assert(mAreaInfoList != null
-            , "mAreaInfoList 인스펙터에서 빠짐");
+        // TODO : 엑셀이나 CSV부터 읽어오기 + 에디터에 올리기
+        public List<AreaInfoSO> AreaInfoList => mAreaInfoList;
+        public List<MonsterType> SpawnMonsterTypeList => mSpawnMonsterTypeList;
+
+        [Header("현재 스테이지")]
+        [SerializeField] private int mStageIndex;
+
+        [Header("몬스터 정보")]
+        [SerializeField] private float mMonsterHp = 100f;
+        [SerializeField] private float mMonsterAttack = 10f;
+        [SerializeField] private List<MonsterType> mSpawnMonsterTypeList;
+    
+        [Header("구역 정보 1구역 2구역 3구역 4구역(보스스테이지)")]
+        [SerializeField] private List<AreaInfoSO> mAreaInfoList;
+    
+        private void OnValidate()
+        {
+            Debug.Assert(mAreaInfoList != null
+                , "mAreaInfoList 인스펙터에서 빠짐");
+        }
     }
 }
