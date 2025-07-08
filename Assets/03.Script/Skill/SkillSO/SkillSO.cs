53fd3d2df1ddfbe2bc1cca4c5d59e4f37fa57bbb
dopajac <ivg07039@naver.com>
2025-07-08 10:09:26 +0900

    [skill list SO 제작]

---
+++ Assets/03.Script/Skill/SkillSO/SkillSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Skill", menuName = "ScriptableObjects/Skills")]
+
+public class SkillSO : ScriptableObject
+{
+    public List<ScriptableObject> skill_List;
+}
