214c51681e5db0aa50fae5fc3457cb1dac449e03
shallsus <shallsuskim@gmail.com>
2025-07-09 17:35:30 +0900

    [test]

--- Assets/03.Script/GameManager.cs
+++ Assets/03.Script/GameManager.cs
@@ -7,6 +7,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public int choiceCount = 3;
+    public OptionButton[] optionButtons; // UI 버튼 배열
+    
+    public enum EOptionType
+    {
+        Skill,
+        Equip,
+        Training
+    }
+    Dictionary<Enum, ScriptableObject> m_Options = new Dictionary<Enum, ScriptableObject>();
+    
     public OptionChoice_SkillOption skillOption;
     public OptionChoice_EquipOption equipOption;
     public OptionChoice_TrainingOption trainingOption;
@@ -32,7 +44,19 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         int[] playerSkillID = { 100012, 100016, 100020 };
+        m_Options.Add(EOptionType.Skill, skillOption);
+        m_Options.Add(EOptionType.Equip, equipOption);
+        m_Options.Add(EOptionType.Training, trainingOption);
         GameStart(playerSkillID);
+        // 오류 저장용 
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateChoices();
+        }
     }
 
     public void GameStart(int[] playerSkillID)
@@ -45,10 +69,11 @@ public class GameManager : MonoBehaviour
     {
         characterID = playerSkillID;
     }
-    // 게임 시작할 때 InGame선택지 ScriptableObject 생성
+
     void SetDatabase()
     {
 #if UNITY_EDITOR
+        // 게임 시작할 때 InGame선택지 ScriptableObject 생성
         // 1. HashSet으로 ID 생성
         HashSet<int> allowedIds = new HashSet<int>(characterID);
         
@@ -66,4 +91,69 @@ public class GameManager : MonoBehaviour
         AssetDatabase.SaveAssets();
 #endif
     }
+    
+    #region 랜덤 선택지 뽑는 코드
+    
+    private void CreateChoices()
+    {
+        for (int i = 0; i < choiceCount; i++)
+        {
+            EOptionType choicedOption = GetOptionType();
+            if (m_Options.TryGetValue(choicedOption, out ScriptableObject option))
+            {
+                switch (choicedOption)
+                {
+                    case EOptionType.Skill:
+                        var skill = option as OptionChoice_SkillOption;
+                        int skillID = GetSelectionID(skill);
+                        optionButtons[i].selectID = skillID;
+                        optionButtons[i].optionType = "Skill";
+                        Debug.Log("Skill 선택지 띄움 SkillOption 번호 : " + skillID + "/" + i);
+                        break;
+                    case EOptionType.Equip:
+                        var equip = option as OptionChoice_EquipOption;
+                        int equipID = GetSelectionID(equip);
+                        optionButtons[i].selectID = equipID;
+                        optionButtons[i].optionType = "Equip";
+                        Debug.Log("Equip 선택지 선택됨 EquipOption 번호: " + equipID + "/" + i);
+                        break;
+                    case EOptionType.Training:
+                        var training = option as OptionChoice_TrainingOption;
+                        int trainingID = GetSelectionID(training);
+                        optionButtons[i].selectID = trainingID;
+                        optionButtons[i].optionType = "Training";
+                        Debug.Log("Training 선택지 선택됨 Training 번호: " + trainingID + "/" + i);
+                        break;
+                }
+            }
+            else
+            {
+                Debug.LogError("No option found for type: " + choicedOption);
+            }
+        }
+    }
+
+    // 3종류 선택지 중에 어떤 선택지를 띄울지 랜덤 선택 (장비 , 스킬, 훈련)
+    EOptionType GetOptionType()
+    {
+        EOptionType[] values = (EOptionType[])System.Enum.GetValues(typeof(EOptionType));
+        int randomIndex = UnityEngine.Random.Range(0, values.Length);
+        return values[randomIndex];
+    }
+    
+    // 선택지 번호를 가져오는 코드
+    int GetSelectionID<T>(ExelReaderBase<T> option) where T : BaseValue, new()
+    {
+        if (option == null || option.data == null || option.data.Count == 0)
+        {
+            Debug.LogWarning("옵션이 null이거나 비어있음");
+            return -1;
+        }
+
+        int randomIndex = UnityEngine.Random.Range(0, option.data.Count);
+        return option.data[randomIndex].Key_ID; // IDValuePair<T> 에 id가 있다고 가정
+    }
+    
+
+    #endregion
 }
