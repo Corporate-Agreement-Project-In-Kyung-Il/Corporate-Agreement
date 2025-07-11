5f8d28ebb0dc6b6c9f3e18def68be47d72847d74
DESKTOP-O34IBGR\KGA <cozam2@naver.com>
2025-07-11 17:33:32 +0900

    콜라이더 사이즈조절 int에서 flaot으로 변경하기

--- Assets/03.Script/Skill/SkillPrefab/AquaBall.cs
+++ Assets/03.Script/Skill/SkillPrefab/AquaBall.cs
@@ -53,12 +53,12 @@ public class AquaBall : ActiveSkillBase, ISkillID
         if (owner.skills[0].SkillID == SkillID && owner.skills[0] is ActiveSkillSO skill)
         {
             stat.Damage = skill.Skill_Damage;
-            coll.size = new Vector2(stat.Range_width, stat.Range_height);
+           // coll.size = new Vector2(stat.Range_width, stat.Range_height);
         }
         else if (owner.skills[1].SkillID == SkillID && owner.skills[1] is ActiveSkillSO skill2)
         {
             stat.Damage = skill2.Skill_Damage;
-            coll.size = new Vector2(stat.Range_width, stat.Range_height);
+            //coll.size = new Vector2(stat.Range_width, stat.Range_height);
         }
     }
 }
