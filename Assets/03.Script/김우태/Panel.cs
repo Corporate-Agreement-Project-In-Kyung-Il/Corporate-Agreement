3cbb95676580c406761915424adbd20d8e10460c
kot775 <kot775@naver.com>
2025-07-24 11:20:20 +0900

    [UI 보강 중]

---
+++ Assets/03.Script/김우태/Panel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour
+{
+    public Text titleText;
+    public Text contentText;
+    public Button closeButton;
+    public Button confirmButton;
+}
