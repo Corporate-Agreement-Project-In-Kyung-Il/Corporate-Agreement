5d4d265a45bfa86d3fd73ee07a79dea7f84a9b79
juno <cozam3@gmail.com>
2025-07-25 11:19:44 +0900

    사운드 시스템 추가 SFXManager와 BGMManager

---
+++ Assets/03.Script/Sound/SFXData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SFXData", menuName = "Sound/SFXData")]
+public class SFXData : ScriptableObject
+{
+    public string soundName;
+    public AudioClip clip;
+    [Range(0, 2)]
+    public float volume = 1f;
+}
---
+++ Assets/03.Script/Sound/SFXManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager Instance;
+
+    [SerializeField]
+    private int poolSize = 10;
+
+    private List<AudioSource> audioSources = new List<AudioSource>();
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            var sources = gameObject.AddComponent<AudioSource>();
+            audioSources.Add(sources);
+        }
+    }
+
+    public void Play(SFXData sfxData)
+    {
+        foreach (var sources in audioSources)
+        {
+            if (sources.isPlaying == false)
+            {
+                sources.clip = sfxData.clip;
+                sources.volume = sfxData.volume;
+     
+
+                sources.Play();
+                return;
+            }
+        }
+    }
+}
