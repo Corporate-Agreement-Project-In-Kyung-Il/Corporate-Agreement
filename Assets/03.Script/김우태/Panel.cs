690b36d32872af3b82863c84c32388ae3a011ca9
kot775 <kot775@naver.com>
2025-08-01 16:04:07 +0900

    [빌드 전 커밋]

--- Assets/03.Script/김우태/Panel.cs
+++ Assets/03.Script/김우태/Panel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Panel : MonoBehaviour
@@ -9,4 +10,9 @@ public class Panel : MonoBehaviour
     public Text contentText;
     public Button closeButton;
     public Button confirmButton;
+
+    public void ReturnTitleScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
