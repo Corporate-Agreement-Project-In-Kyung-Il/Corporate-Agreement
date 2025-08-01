5ed81a3637e105b287f4314ab3fd26ad4022b9d7
dopajac <ivg07039@naver.com>
2025-08-01 15:12:01 +0900

    [이계 스킬 패널 ONOFF]

--- Assets/03.Script/이주노/MainMenuButton.cs
+++ Assets/03.Script/이주노/MainMenuButton.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using BackEnd.Quobject.SocketIoClientDotNet.Client;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButton : MonoBehaviour
 {
+    [SerializeField] private GameObject EsekaiSkillPanel;
     public void LoadScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OnOffEsekaiSkillPanel()
+    {
+        if (EsekaiSkillPanel.activeSelf)
+        {
+            EsekaiSkillPanel.SetActive(false);
+        }
+        else 
+        {
+            EsekaiSkillPanel.SetActive(true);
+        }
     }
 }
