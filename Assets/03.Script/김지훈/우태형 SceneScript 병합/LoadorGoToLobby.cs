b0456b3cb3c11a1622ca1e14cc51d16777710c3b
shallsus <shallsuskim@gmail.com>
2025-08-04 10:59:48 +0900

    1. Lobby 기능 넣음

--- Assets/03.Script/김지훈/우태형 SceneScript 병합/LoadorGoToLobby.cs
+++ Assets/03.Script/김지훈/우태형 SceneScript 병합/LoadorGoToLobby.cs
@@ -9,11 +9,10 @@ public class LoadorGoToLobby : MonoBehaviour
 {
     //0번은 이어하기, 1번은  로비로 돌아가기
     private Button[] buttons;
-    private PlayerData[] players;
+    private Player[] players;
 
     private void Start()
     {
-        players = GameManager.Instance.playerStatAdjust.Data;
         buttons = GetComponentsInChildren<Button>();
         for (int i = 0; i < buttons.Length; i++)
         {
@@ -26,22 +25,23 @@ public class LoadorGoToLobby : MonoBehaviour
     {
         for (int i = 0; i < players.Length; i++)
         {
-            TSVLoaderSample.OverwritePlayerData(players[i]);
+            TSVLoaderSample.OverwritePlayerData(players[i].data);
         }
         
         if (index.Equals(0))
         {
             //살아나
-            StageEvent.OnTriggerStageClearEvent();
-            gameObject.SetActive(false);
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i].initialSetPlayerStats(players[i].data);
+                players[i].gameObject.SetActive(true);
+                players[i].ChangeState(CharacterState.Run);
+            }
         }
         else
         {
             //돌아가
-            SceneManager.LoadScene(0);
-            Destroy(AliveExistSystem.Instance.gameObject);
-            Destroy(PlayerList.Instance.gameObject);
-            Destroy(GameManager.Instance.gameObject);
+            GameManager.Instance.LoadScene(0, this.gameObject);
         }
     }
 }
